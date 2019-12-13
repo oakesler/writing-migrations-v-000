13e2f28d8e5274243e06eece1c794e4653b9ac0c
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 06:42:28 +0000

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,15 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   
   def change
-    change_column(table_name, column_name, type)
+    change_column :students (table_name, column_name, type)
   end
 end
+
+add_column :students do |t|
+      t.integer :grade 
+      t.string :birthdate
+      #t.string :genre
+      #t.integer :age
+      #t.string :hometown
+    end
+  end
